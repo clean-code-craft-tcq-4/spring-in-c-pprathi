3552ba2dba4d15d837478d5b0728231155e60ed4
Prathibha Ponnuswamy <pprathi.01@gmail.com>
2022-05-22 21:48:20 +0530

    updated stats.c

--- stats.c
+++ stats.c
@@ -11,29 +11,41 @@ struct Stats compute_statistics(const float* numberset, int setlength)
     struct Stats s;
     int idx = 0;                                //Variable to iterate the numberset
     float sum = 0;                              //Variable to hold the sum
-    //Initialize the min and max
-    s.min = *numberset;
-    s.max = *numberset;
-    //Determine the min, max and sum of the numberset
-    for (idx = 0; idx < setlength; idx++)
-        {
-            sum += *numberset;
-            if (s.min > *numberset)
+    s.min = 0;
+    s.max = 0;
+    s.average = 0;
+    if(setlength > 0)
+    {
+        //Initialize the min and max
+        s.min = *numberset;
+        s.max = *numberset;
+        //Determine the min, max and sum of the numberset
+        for (idx = 0; idx < setlength; idx++)
             {
-                s.min = *numberset;
+                sum += *numberset;
+                if (s.min > *numberset)
+                {
+                    s.min = *numberset;
+                }
+                else if(s.max < *numberset)
+                {
+                    s.max = *numberset;
+                }
+                else
+                {
+                    //do nothing
+                }
+                numberset++;
             }
-            else if(s.max < *numberset)
-            {
-                s.max = *numberset;
-            }
-            else
-            {
-                //do nothing
-            }
-            numberset++;
-        }
-    //Determine the average
-    s.average = sum/setlength;
+        //Determine the average
+        s.average = sum/setlength;
+    }
+    else
+    {
+        s.min = s.min/setlength;
+        s.max = s.max/setlength;
+        s.average = s.average/setlength;
+    }
 
     return s;
 }
